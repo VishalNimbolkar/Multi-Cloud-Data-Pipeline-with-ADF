dc6fec44b424930883996f4f1bb99830c2444d4d
Vishal Nimbolkar <80448632+VishalNimbolkar@users.noreply.github.com>
2025-09-21 21:01:38 +0530

    Create premium_orders_table.sql

---
+++ SQL/premium_orders_table.sql
@@ -0,0 +1,12 @@
+
+-- In database create table using below schema
+
+  
+CREATE TABLE premium_orders (
+order_id INT NOT NULL,
+order_date VARCHAR(45) NOT NULL,
+order_customer_id INT NOT NULL,
+order_status VARCHAR(45) NOT NULL,
+order_amount FLOAT,
+PRIMARY KEY (order_id)
+);
