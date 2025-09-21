2f775ac38b11ef7857ff869c294a60a814908e77
Vishal Nimbolkar <80448632+VishalNimbolkar@users.noreply.github.com>
2025-09-21 21:01:59 +0530

    Update premium_orders_table.sql

--- SQL/premium_orders_table.sql
+++ SQL/premium_orders_table.sql
@@ -5,7 +5,7 @@
 CREATE TABLE premium_orders (
 order_id INT NOT NULL,
 order_date VARCHAR(45) NOT NULL,
-order_customer_id INT NOT NULL,
+customer_id INT NOT NULL,
 order_status VARCHAR(45) NOT NULL,
 order_amount FLOAT,
 PRIMARY KEY (order_id)
